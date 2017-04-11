a6be08f21cd29219bbc66c45ecd82a8b5b3bdd98
Mate Antunovic <just.mate.antunovic@gmail.com>
2017-04-11 21:45:24 +1200

    Make report better

--- weather.py
+++ weather.py
@@ -10,17 +10,23 @@ class WeatherReport:
     url = ''.join([metservice.METSERVICE_BASE, metservice.API_OPTIONS[forecast_kind].format(city)])
     self.report = metservice.get_response(url)
 
-  def morning_temp(self):
-    return (0, 0)
+  def get_days(self):
+    pass
 
-  def midday_temp(self):
-    return (0, 0)
-
-  def evening_temp(self):
-    return (0, 0);
+class DayReport:
+  def __init__(self, day_report):
+    self.day_report = day_report;
 
   def brief_summary(self):
-    return "Rain, followed by rain"
+    return self.day_report["forecast"]
 
   def full_summary(self):
-    return self.brief_summary()
+    high_temp= self.day_report["max"]
+    min_temp = self.day_report["min"]
+    temp_forecast = "High of %sC and low of %sC." % (high_temp, low_temp)
+    full_day_report = "%s. %s" % (temp_forcast, self.brief_summary())
+
+    for period in ["morning", "afternoon", "evening", "overnight"]:
+      full_day_report = "%s\n%s: %s." % (full_day_report, period, self.day_report["partDayData"][period])
+
+    return full_day_report
